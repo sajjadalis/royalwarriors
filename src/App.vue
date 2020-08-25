8863a2ec46aa11fe3a6b55045db1755eeb24bb50
Sajjad Ali <sajjadalis@gmail.com>
2020-08-26 03:36:05 +0500

    Tailwindcss setup with vue-cli

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,7 @@ import Home from '@/components/Home'
 
 export default {
   name: 'App',
-  // components: {
-  //   Home
-  // },
-  // data() {
-  //   return{
-	//   lvl2Monsters: null,
-	//   lvl3Monsters: null
-  //   }
-  // },
-  // methods: {
-  //   sum(items, prop){
-	//   return items.reduce( function(a, b)
-	//   {
-  //         return a + b[prop];
-  //     }, 0);
-  //   }
-  // },
-  // mounted() {
-  //   let cases = "http://localhost:8080/MonsterHuntingToday.csv";
-
-  //           this.$papa.parse(cases, {
-  //               header: true,
-  //               download: true,
-  //               dynamicTyping: true,
-  //               skipEmptyLines: true,
-  //               complete: (results) => {
-  //                   console.log(results);
-	// 				this.lvl2Monsters = this.sum(results.data, 'Lvl 2');
-	// 				this.lvl3Monsters = this.sum(results.data, 'Lvl 3');
-	// 				console.log(this.lvl2Monsters);
-  //               }
-  //           })
-  // }
 }
 </script>
+
+<style src="./assets/tailwind.css">
