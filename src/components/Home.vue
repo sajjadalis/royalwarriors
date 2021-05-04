f2ef0852f5a8a4eb6ea3bec942da168d305437b2
Sajjad Ali <6752546+sajjadalis@users.noreply.github.com>
2021-05-04 08:04:00 +0500

    Changed guildname

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 
         <div class="relative">
             <img src="assets/header.jpg" class="w-full h-auto mb-5 md:mb-10 rounded-b-lg">
-            <h1 class="absolute top-0 left-0 font-heading font-bold text-xs md:text-3xl text-white mb-5 md:mb-10 leading-tight py-1 px-2 md:py-5 md:px-10 mt-2 md:mt-12 bg-black bg-opacity-75">The Royal Warriors Hunting Report for {{ today }}</h1>
+            <h1 class="absolute top-0 left-0 font-heading font-bold text-xs md:text-3xl text-white mb-5 md:mb-10 leading-tight py-1 px-2 md:py-5 md:px-10 mt-2 md:mt-12 bg-black bg-opacity-75">[HL4]BOPOMOFO Hunting Report for {{ today }}</h1>
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-5 md:gap-10">
